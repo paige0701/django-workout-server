f4bac81f25a1331837586865b53af37c9e36389a
Paige <paigechoi0701@gmail.com>
2021-11-23 17:11:44 +0900

    update dockerfile, docker-compose - add nginx, gunicorn for running

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,20 @@
 # syntax=docker/dockerfile:1
 FROM python:3
+
+RUN apt-get -y update && apt-get -y dist-upgrade
+RUN apt-get install -y apt-utils dialog libpq-dev
+
+RUN apt-get install -y python3-pip python3-dev
 ENV PYTHONUNBUFFERED=1
-WORKDIR /code
-COPY requirements.txt /code/
-RUN pip install -r requirements.txt
-COPY . /code/
+ENV PYTHONDONTWRITEBYTECODE 1
+# pip setuptools 업그레이드
+RUN pip3 install --upgrade pip
+RUN pip3 install --upgrade setuptools
+
+RUN mkdir /src
+ADD ./requirements.txt /src/
+
+RUN pip3 install -r /src/requirements.txt
+
+WORKDIR /src
+COPY . /src
--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -28,10 +28,10 @@ for key, value in secrets.items():
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d(-ljqo5d9t7+$cqhyc&uhq9itqz$u1yr$we!hco^p!ysw#(xo'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'this-is-secret-code')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
@@ -146,7 +146,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
